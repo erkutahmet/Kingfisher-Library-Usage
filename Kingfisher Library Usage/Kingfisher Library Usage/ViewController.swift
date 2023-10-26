705b54912045333ce68cd2a06c05e5b94c538347
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-26 03:18:12 +0300

    Kingfisher Library Usage (set image from url)

--- Kingfisher Library Usage/Kingfisher Library Usage/ViewController.swift
+++ Kingfisher Library Usage/Kingfisher Library Usage/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showImage(imageName: "kofte.png")
     }
 
-
+    @IBAction func buttonImage1(_ sender: Any) {
+        showImage(imageName: "ayran.png")
+    }
+    
+    @IBAction func buttonImage2(_ sender: Any) {
+        showImage(imageName: "su.png")
+    }
+    
+    func showImage(imageName: String) {
+        if let url = URL(string: "http://kasimadalan.pe.hu/yemekler/resimler/\(imageName)") {
+            DispatchQueue.main.async {
+                self.imageView.kf.setImage(with: url)
+            }
+        }
+    }
 }
 
